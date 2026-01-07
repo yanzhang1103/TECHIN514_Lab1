12e337c11ff9dfffcd7cd4945d03ce2608fa398c
yanzhang1103 <zy901103@uw.edu>
2026-01-07 15:35:26 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
-#include <Arduino.h>
-
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("System Initialized...");
-}
-
-void loop() {
-  Serial.println("Hello World! XIAO ESP32C3 is running.");
-  delay(1000);
-}
+/*  hello_xiao    A first sketch for the Seeed Studio XIAO ESP32C3 in PlatformIO*
+  
+    *This example code is in the public domain.*
+  
+    *Michel Deslierres  June 15, 2020 */
+  
+  #include <Arduino.h>  // needed in PlatformIO*
+  
+  void setup() {
+    Serial.begin(115200);
+  }
+  
+  void loop() {
+    Serial.println("Hello XIAO!");
+    delay(2000);
+  }
